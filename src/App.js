9f72b2bcf25e51e19dfe38eb8577f12b57223190
tina <oge612@gmail.com>
2022-03-10 14:02:03 +0100

    omdb api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./Card";
+import searchIcon from "./search.svg";
+// 668c2a95
+const API_URL = "http://www.omdbapi.com?apikey=668c2a95";
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+
+  // fetch movies
+  const fetchMovies = async (title) => {
+    const res = await fetch(`${API_URL}&s=${title}`);
+    const data = await res.json();
+    setMovies(data.Search);
+  };
+  useEffect(() => {
+    fetchMovies("");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>OMDB</h1>
+
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for movies"
+          value={search}
+          onChange={(evt) => {
+            setSearch(evt.target.value);
+          }}
+        />
+        <img
+          src={searchIcon}
+          alt="search"
+          onClick={() => {
+            fetchMovies(search);
+          }}
+        />
+      </div>
+   
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie,i) => (
+            <Card movie={movie} key={i}/>
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 }
